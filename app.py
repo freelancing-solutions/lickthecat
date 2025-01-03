86da493de966cdc2e30f07348ccf827ac5928957
mothetho <mobiusndou@gmail.com>
2025-01-03 23:18:25 +0200

    initdsad

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from src.config import config_instance
 
 if __name__ == "__main__":
     app = create_app(config_instance())
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=8015)
